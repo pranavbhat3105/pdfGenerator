75926c57c9f75dda4124ee8f1e2a98fc1ead739f
Pranav Bhat <pranavbhat3105@gmail.com>
2021-08-12 18:22:31 +0530

    First Commit

---
+++ Server/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const pdf = require('html-pdf');
+const cors = require('cors');
+
+const pdfTemplate = require('./documents');
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.post('/create-pdf',(req, res) => {
+    
+    //We create a pdf where we get the data from state and it will be filled in the pdf wiht the help of toFile funciton which takes the name of the file as an parameter.
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
+        if(err) {
+            res.send(Promise.reject());
+        }
+        res.send(Promise.resolve());
+    });
+});
+
+
+// We will fetch the pdf with GET and it will download in the directory with the name of result.pdf
+app.get('/fetch-pdf', (req, res) => {
+    res.sendFile(`${__dirname}/result.pdf`)
+})
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
